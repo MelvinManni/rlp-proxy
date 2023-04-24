c9143994f85bf92eb74961f44fbc1a33d809fe31
Melvin <melvinnnamani@gmail.com>
2023-04-24 19:59:57 +0100

    fixed redis and project env

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const port = Number(process.env.PORT || 8080);
 
 if (process.env.REDISTOGO_URL) {
   var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  
   var redis = require('redis').createClient(rtg.port, rtg.hostname);
 
   redis.auth(rtg.auth.split(':')[1]);
